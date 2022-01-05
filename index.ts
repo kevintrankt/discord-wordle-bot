495df94a54ac6427aa9a264f40846bcfca0aa8cf
Kevin Tran <kevin.tran.kt@gmail.com>
2022-01-04 19:46:01 -0800

    Major scoreboard updates

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-import DiscordJS, { Intents } from 'discord.js'
+import DiscordJS, { Intents, MessageEmbed } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
-
-// https://discord.com/api/oauth2/authorize?client_id=928012714025042001&permissions=8&scope=bot%20applications.commands
 
 const client = new DiscordJS.Client({
     intents: [
@@ -30,6 +28,9 @@ const loadScoreBoard = () => {
             }
             scoreboard = JSON.parse(data.toString());
             console.log(scoreboard);
+            for (const id in scoreboard) {
+                client.users.fetch(id)
+            }
             console.log('Bot Ready!')
         });
     } else {
@@ -50,21 +51,64 @@ const writeScoreBoard = () => {
     })
 }
 
-const updateScoreBoard = (user: any, points: number) => {
+const updateScoreBoard = (userid: any, points: number) => {
 
     // create a new scoreboard entry
-    if (!scoreboard[user.username]) {
-        scoreboard[user.username] = points;
+    if (!scoreboard[userid]) {
+        scoreboard[userid] = points;
     } else {
-        scoreboard[user.username] += points;
+        scoreboard[userid] += points;
     }
 
     writeScoreBoard();
 
-    return scoreboard[user.username];
+    return scoreboard[userid];
+}
+
+const generateScoreBoardMessage = () => {
+    // sort results
+    let scoreArray = []
+    for (const key in scoreboard) {
+        scoreArray.push([key, scoreboard[key]]);
+    }
+    scoreArray = scoreArray.sort(sort2d);
+
+    // generate reply
+    let scoreReply = '';
+    for (const entry of scoreArray) {
+        scoreReply += `${client.users.cache.get(entry[0])}: ${entry[1]}\n`
+    }
+
+    return scoreReply;
+}
+
+const generateScoreBoardEmbed = () => {
+    // sort results
+    let scoreArray = []
+    for (const key in scoreboard) {
+        scoreArray.push([key, scoreboard[key]]);
+    }
+    scoreArray = scoreArray.sort(sort2d);
+
+    // generate list of users
+    let userListEmbed = '';
+    for (const entry of scoreArray) {
+        userListEmbed += `${client.users.cache.get(entry[0])}\n`
+    }
+
+    // generate list of scores
+    let scoreListEmbed = '';
+    for (const entry of scoreArray) {
+        scoreListEmbed += `${entry[1]}\n`
+    }
+
+    return [userListEmbed, scoreListEmbed]
 }
 
 client.on('messageCreate', (message) => {
+
+    // Check if admin
+    const isAdmin = message.member?.permissions.has("ADMINISTRATOR");
 
     // Check for Wordle Score
     const wordleRegex = /Wordle \d{3} ([\dX])\/6\n{0,2}[⬛🟩🟨⬜]{5}/;
@@ -82,7 +126,7 @@ client.on('messageCreate', (message) => {
             wordleScore = 7 - parseInt(wordleMessage[1]);
         }
 
-        const updatedScore = updateScoreBoard(message.author, wordleScore);
+        const updatedScore = updateScoreBoard(message.author.id, wordleScore);
 
         // reply with score
         message.reply({
@@ -93,25 +137,57 @@ client.on('messageCreate', (message) => {
 
     // leaderboard command
     if (message.content === '!w scores') {
-        let testUser = message.guild?.members.cache.get('572204153674137600');
-
-        // sort results
-        let scoreArray = []
-        for (const key in scoreboard) {
-            scoreArray.push([key, scoreboard[key]]);
-        }
-        scoreArray = scoreArray.sort(sort2d);
-
-        // generate reply
-        let scoreReply = '';
-        for (const entry of scoreArray) {
-            scoreReply += `${entry[0]}: ${entry[1]}\n`
-        }
-
-        message.reply({
-            content: `${scoreReply}`
-        })
+        const exampleEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(':green_square: :green_square: :green_square: LEADERBOARD :green_square: :green_square: :green_square:')
+            .setURL('https://www.powerlanguage.co.uk/wordle/')
+            .addFields(
+                { name: 'User', value: generateScoreBoardEmbed()[0], inline: true },
+                { name: 'Score', value: generateScoreBoardEmbed()[1], inline: true }
+            )
+        message.channel.send({ embeds: [exampleEmbed] })
     }
+
+
+    // admin commands 
+    if (isAdmin) {
+
+        // just debug stuff
+        if (message.content === '!w debug') {
+            const exampleEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(':green_square: :green_square: :green_square: NEW WORDLE CHALLENGE :green_square: :green_square: :green_square:')
+                .setURL('https://www.powerlanguage.co.uk/wordle/')
+                .addFields(
+                    { name: 'User', value: generateScoreBoardEmbed()[0], inline: true },
+                    { name: 'Score', value: generateScoreBoardEmbed()[1], inline: true }
+                )
+            message.channel.send({ embeds: [exampleEmbed] })
+        }
+
+        // manual update score
+        if (message.content.includes('!w x')) {
+            const updateRegex = /!(\d+)>([+-]\d)/
+            const updateParse = message.content.match(updateRegex);
+
+            if (updateParse) {
+                console.log(updateParse);
+                const userid = updateParse[1];
+                const score = parseInt(updateParse[2]);
+                const updatedScore = updateScoreBoard(userid, score);
+
+                // reply with score
+                message.reply({
+                    content: `${score} points for ${client.users.cache.get(userid)} (${updatedScore} total points)`
+                })
+
+            }
+
+        }
+
+    }
+
+
 })
 
 const sort2d = (a: any, b: any) => {
